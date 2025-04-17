0676be5182ecbed833c2950cd8aed9601345c1d1
AkashSingh2218271 <akrawat0056@gmail.com>
2025-04-17 10:30:52 +0530

    Adding code for seprating tags form an htmlfile and other codes to remove comments from a file and store code into new file

---
+++ VI SEM/Compiler Design/TestOne.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+// code to add two numbers
+
+/*
+    a = 5,
+    b = 5
+    printsum
+*/
+
+int main() {
+    int a = 5;
+    int b = 5;// this is the code
+
+    int sum = a + b;
+    printf("sum is %s", sum);
+    
+    return 0;
+}
--- VI SEM/Compiler Design/lex.yy.c
+++ VI SEM/Compiler Design/lex.yy.c
@@ -282,12 +282,15 @@ static void yy_fatal_error YY_PROTO(( yyconst char msg[] ));
 	*yy_cp = '\0'; \
 	yy_c_buf_p = yy_cp;
 
-#define YY_NUM_RULES 4
-#define YY_END_OF_BUFFER 5
-static yyconst short int yy_accept[20] =
+#define YY_NUM_RULES 6
+#define YY_END_OF_BUFFER 7
+static yyconst short int yy_accept[44] =
     {   0,
-        2,    2,    5,    2,    3,    2,    2,    2,    2,    2,
-        2,    2,    2,    2,    2,    2,    1,    1,    0
+        0,    0,    7,    4,    5,    4,    4,    4,    4,    1,
+        4,    4,    0,    4,    4,    1,    1,    1,    4,    4,
+        4,    0,    2,    4,    1,    1,    1,    1,    1,    2,
+        4,    1,    3,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    0
     } ;
 
 static yyconst int yy_ec[256] =
@@ -296,16 +299,16 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    3,    1,    3,    3,    3,
-        3,    3,    3,    3,    3,    3,    3,    1,    1,    1,
-        1,    1,    1,    4,    1,    1,    1,    1,    1,    1,
+        1,    3,    1,    1,    1,    1,    4,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    5,    5,    6,    5,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
 
-        5,    5,    5,    5,    5,    5,    5,    5,    7,    5,
-        8,    5,    5,    5,    5,    5,    5,    5,    5,    5,
-        5,    5,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -322,41 +325,47 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static yyconst int yy_meta[9] =
+static yyconst int yy_meta[5] =
     {   0,
-        1,    2,    1,    1,    1,    1,    1,    1
+        1,    2,    1,    1
     } ;
 
-static yyconst short int yy_base[24] =
+static yyconst short int yy_base[47] =
     {   0,
-       36,    0,   30,    0,   39,    4,    0,    8,   12,   20,
-       16,    8,    3,   23,    3,   26,    0,    1,   39,   33,
-       35,    5,    2
+        0,    0,   50,   45,   51,    4,   44,   44,    8,   12,
+       16,    0,   43,   41,   18,   34,   22,    0,    0,   24,
+       26,   28,    0,    0,   32,    0,    0,   20,   36,    0,
+        0,    0,   51,   38,   14,    0,    0,    0,    0,    0,
+        0,    0,   51,   13,   42,    5
     } ;
 
-static yyconst short int yy_def[24] =
+static yyconst short int yy_def[47] =
     {   0,
-       20,   21,   19,   22,   19,   23,   22,   23,   19,   19,
-       22,   10,   22,   10,   22,   10,   22,   10,    0,   19,
-       19,   19,   19
+       43,    1,   43,   44,   43,   43,   44,    6,   43,   43,
+       43,    9,   45,    9,    9,   46,   43,   10,   11,   11,
+       43,   45,   21,   14,   43,   10,   17,   17,   43,   21,
+       20,   29,   43,   25,   34,   25,   29,   28,   29,   35,
+       25,   29,    0,   43,   43,   43
     } ;
 
-static yyconst short int yy_nxt[48] =
+static yyconst short int yy_nxt[56] =
     {   0,
-        4,    5,    8,    4,    7,    7,   14,    9,    7,   17,
-       15,    9,    7,   14,    7,    7,   10,   10,   10,   10,
-       11,   13,   11,   11,   12,   12,   12,   12,   14,   19,
-       16,   14,   18,    4,    4,    6,    6,    5,    3,   19,
-       19,   19,   19,   19,   19,   19,   19
+        4,    5,    4,    6,    7,   16,    9,   10,   12,   13,
+       14,   15,   16,    7,   17,   18,   19,   42,   20,   21,
+       24,   25,   27,   37,   28,   29,   19,   30,   31,   32,
+       22,   33,   34,   13,   35,   36,   27,   26,   38,   39,
+       40,   41,   13,   13,   23,   22,   11,    8,    8,   43,
+        3,   43,   43,   43,   43
     } ;
 
-static yyconst short int yy_chk[48] =
+static yyconst short int yy_chk[56] =
     {   0,
-        2,    2,   23,    2,    6,   22,   18,    6,    8,   15,
-       13,    8,    9,   12,    9,    9,    9,    9,    9,    9,
-       10,   11,   10,   10,   10,   10,   10,   10,   14,    3,
-       14,   16,   16,   20,   20,   21,   21,    1,   19,   19,
-       19,   19,   19,   19,   19,   19,   19
+        1,    1,    1,    1,    6,   46,    6,    6,    9,    9,
+        9,    9,   10,   44,   10,   10,   11,   35,   11,   11,
+       15,   15,   17,   28,   17,   17,   21,   20,   21,   21,
+       22,   22,   25,   25,   25,   25,   29,   16,   29,   29,
+       34,   34,   45,   45,   14,   13,    8,    7,    4,    3,
+       43,   43,   43,   43,   43
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -370,11 +379,12 @@ static char *yy_last_accepting_cpos;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 ".\\EmailValidOrNot.l"
+#line 1 ".\\RemoveCommentsAdvance.l"
 #define INITIAL 0
-#line 2 ".\\EmailValidOrNot.l"
+#line 2 ".\\RemoveCommentsAdvance.l"
     #include <stdio.h>
-#line 378 "lex.yy.c"
+    #include <string.h>
+#line 388 "lex.yy.c"
 
 /* Macros after this point can all be overridden by user definitions in
  * section 1.
@@ -517,9 +527,6 @@ YY_MALLOC_DECL
 #endif
 
 #define YY_RULE_SETUP \
-	if ( yyleng > 0 ) \
-		yy_current_buffer->yy_at_bol = \
-				(yytext[yyleng - 1] == '\n'); \
 	YY_USER_ACTION
 
 YY_DECL
@@ -528,9 +535,9 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
 
-#line 5 ".\\EmailValidOrNot.l"
+#line 6 ".\\RemoveCommentsAdvance.l"
 
-#line 534 "lex.yy.c"
+#line 541 "lex.yy.c"
 
 	if ( yy_init )
 		{
@@ -569,7 +576,6 @@ YY_DECL
 		yy_bp = yy_cp;
 
 		yy_current_state = yy_start;
-		yy_current_state += YY_AT_BOL();
 yy_match:
 		do
 			{
@@ -582,13 +588,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 20 )
+				if ( yy_current_state >= 44 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 39 );
+		while ( yy_base[yy_current_state] != 51 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -616,25 +622,52 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 6 ".\\EmailValidOrNot.l"
-{ printf("%s is a valid email address", yytext); }
+#line 7 ".\\RemoveCommentsAdvance.l"
+{
+    char *comment_pos = strstr(yytext, "//");
+    if (comment_pos != NULL) {
+        *comment_pos = '\0'; // Truncate at //
+        fprintf(yyout, "%s\n", yytext); // Print code before //
+    }
+}
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 7 ".\\EmailValidOrNot.l"
-{ printf("%s is not a valid email address", yytext); }
+#line 14 ".\\RemoveCommentsAdvance.l"
+{
+    char temp[1024];
+    strcpy(temp, yytext);
+    char *start = strstr(temp, "/*");
+    char *end = strstr(temp, "*/");
+    if (start && end && end > start) {
+        *start = '\0'; // Truncate before block comment
+        fprintf(yyout, "%s\n", temp); // Print code before comment
+    } else {
+        fprintf(yyout, "%s\n", yytext); // fallback
+    }
+}
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 8 ".\\EmailValidOrNot.l"
-{ return 0; }
+#line 26 ".\\RemoveCommentsAdvance.l"
+{ /* ignore block comment */ }
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
-#line 9 ".\\EmailValidOrNot.l"
+#line 27 ".\\RemoveCommentsAdvance.l"
+{ fprintf(yyout, "%s", yytext); }  // Print normal code
+	YY_BREAK
+case 5:
+YY_RULE_SETUP
+#line 28 ".\\RemoveCommentsAdvance.l"
+{ fprintf(yyout, "\n"); }          // Preserve line breaks
+	YY_BREAK
+case 6:
+YY_RULE_SETUP
+#line 30 ".\\RemoveCommentsAdvance.l"
 ECHO;
 	YY_BREAK
-#line 638 "lex.yy.c"
+#line 671 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -914,7 +947,6 @@ static yy_state_type yy_get_previous_state()
 	register char *yy_cp;
 
 	yy_current_state = yy_start;
-	yy_current_state += YY_AT_BOL();
 
 	for ( yy_cp = yytext_ptr + YY_MORE_ADJ; yy_cp < yy_c_buf_p; ++yy_cp )
 		{
@@ -927,7 +959,7 @@ static yy_state_type yy_get_previous_state()
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 20 )
+			if ( yy_current_state >= 44 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -962,11 +994,11 @@ yy_state_type yy_current_state;
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 20 )
+		if ( yy_current_state >= 44 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 19);
+	yy_is_jam = (yy_current_state == 43);
 
 	return yy_is_jam ? 0 : yy_current_state;
 	}
@@ -1085,7 +1117,6 @@ static int input()
 	*yy_c_buf_p = '\0';	/* preserve yytext */
 	yy_hold_char = *++yy_c_buf_p;
 
-	yy_current_buffer->yy_at_bol = (c == '\n');
 
 	return c;
 	}
@@ -1522,13 +1553,18 @@ int main()
 	return 0;
 	}
 #endif
-#line 9 ".\\EmailValidOrNot.l"
+#line 30 ".\\RemoveCommentsAdvance.l"
 
 
 int main() {
-    printf("Enter a sting to check email or not ");
+    yyin = fopen("TestOne.c", "r");
+    yyout = fopen("WithOutComment.c", "w");
     yylex();
 
+    fclose(yyin);
+    fclose(yyout);
+
+    printf("Task done\n");
     return 0;
 }
 
